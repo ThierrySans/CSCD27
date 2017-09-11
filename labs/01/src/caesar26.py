e526ad35cf6bafdc75c4555cad8c0ef2cb43d1ee
Thierry Sans <thierry.sans@gmail.com>
2017-09-11 09:39:05 -0400

    caesar better starter

--- labs/01/src/caesar26.py
+++ labs/01/src/caesar26.py
@@ -1,16 +1,20 @@
 ''' encrypts the plaintext with a key
     based on the caesar cipher algorithm
+    and return the ciphertext
     (string, string) -> string
     REQ: key matches [0-9]*
     REQ: plaintext matches [a-z]*
 '''
 def encrypt(key, plaintext):
-    return plaintext
+    ciphertext = plaintext # dummy instruction
+    return ciphertext
 
-''' decrypts the plaintext with a key
+''' decrypts the ciphertext with a key
     based on the caesar cipher algorithm
+    and returns the plaintext
     (string, string) -> string
     REQ: key matches [0-9]*
     REQ: ciphertext matches [a-z]*'''    
 def decrypt(key, ciphertext):
-    return ciphertext
+    plaintext = ciphertext # dummy instruction
+    return plaintext
