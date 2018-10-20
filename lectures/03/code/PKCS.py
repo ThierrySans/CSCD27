f8be29dcd29d501eee429dd40e970d77b2056a42
Thierry Sans <thierry.sans@gmail.com>
2018-10-19 22:24:51 -0400

    bug in pkcs

--- lectures/03/code/PKCS.py
+++ lectures/03/code/PKCS.py
@@ -6,8 +6,8 @@ from Crypto.PublicKey import RSA
 
 # generate pair of keys (and export them)
 rsa_key = RSA.generate(2048)
-privkey = rsa_key.exportKey()
-pubkey = rsa_key.exportKey(pkcs=8)
+privkey = rsa_key.exportKey(pkcs=8)
+pubkey = rsa_key.publickey().export_key()
 
 from Crypto.Cipher import PKCS1_v1_5
 
